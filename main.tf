af5b403088f6410dfe2afe4d0a7dba2d33a7bc60
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 16:59:01 -0600

    v10

--- main.tf
+++ main.tf
@@ -11,23 +11,33 @@ module "resource_group_1" {
 module "vnet_1" {
   source              = "./modules/vnets"
   vnet_name           = "vnet-ejemplo"
-  resource_group_name = module.resource_group_1.resource_group_name     # ✅
-  location            = module.resource_group_1.resource_group_location # ✅
+  # manda a llamar el nombre del resource group creado en el modulo resource_group_1
+  resource_group_name = module.resource_group_1.resource_group_name
+  location            = module.resource_group_1.resource_group_location
   address_space       = ["20.0.0.0/16"]
+  dns_servers         = []
+  tags                = {
+    environment = "poc"
+    project     = "xpe"
+  }
 
+  
   subnets = {
     subnet1 = {
       address_prefix    = "20.0.10.0/24"
       service_endpoints = ["Microsoft.Storage", "Microsoft.Sql"]
     }
-  }
 
-  tags = {
-    environment = "poc"
-    project     = "xpertal"
   }
 
   providers = {
     azurerm = azurerm.xpe_shared_poc
   }
+
+  depends_on = [module.resource_group_1]
+
+
 }
+
+
+
--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -1,14 +1,15 @@
 output "resource_group_name" {
-    description = "The name of the resource group."
-    value       = azurerm_resource_group.this.name
+  value       = module.resource_group.resource_group_name
+  description = "Resource group name"
+}
+
+output "resource_group_id" {
+  value       = module.resource_group.resource_group_id
+  description = "Resource group id"
 }
 
 output "resource_group_location" {
-    description = "The location of the resource group."
-    value       = azurerm_resource_group.this.location
+  value       = module.resource_group.resource_group_location
+  description = "Resource group location"
 }
 
-output "id" {
-    description = "The ID of the resource group."
-    value       = azurerm_resource_group.this.id
-}
