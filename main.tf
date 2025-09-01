c1a3053d0a4e0354fbbdc6f6753ccc1d81e187bb
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 10:32:38 -0600

    se añade nuevo resource group

--- main.tf
+++ main.tf
@@ -13,3 +13,13 @@ output "resource_group_id" {
   value = module.resource_group1.id
 }
 
+
+module "resource_group2" {
+  source = "./azure/modules/resource_group"
+  providers = { azurerm = xpe_shared_poc
+  }
+  resourece_group_name = "myResourceGroup2"
+  location = "East US"
+  tags = {Environment = "Production", "Owner" = "TeamA"}
+  
+}
