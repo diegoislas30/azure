aa60de9ada069879a9cca62055fe8d8fe2213599
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 12:17:05 -0600

    vamos a ver

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
-output "resource_group" {
-  value = module.resource_group_1.id
+output "resource_group_1" {
+  value = module.resource_group_1.resource_group_name
+  description = "Resource group name"
+}
+
+output "resource_group_1_id" {
+  value = module.resource_group_1.resource_group_id
+  description = "Resource group id"
+}
+
+output "resource_group_1_location" {
+  value = module.resource_group_1.resource_group_location
+  description = "Resource group location"
 }
