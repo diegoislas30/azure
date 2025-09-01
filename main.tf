b9711a34ac79546eca38ef0f9aa6b8fa61e568ca
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 11:56:55 -0600

    v6

--- main.tf
+++ main.tf
@@ -1,28 +1,13 @@
-
-module "resource_group1" {
-  source = "./azure/modules/resource_group"
-  providers = { azurerm = azurerm.xpe_shared_poc
+module "resource_group" {
+  source             = "./modules/resource_group"
+  providers = {
+    azurerm = azurerm.xpe_shared_poc
   }
-  resourece_group_name = "myResourceGroup"
-  location = "East US"
-  tags = {Environment = "Production", "Owner" = "TeamA"}
-  
-}
-
-output "resource_group_id" {
-  value = module.resource_group1.id
-}
-
-
-module "resource_group2" {
-  source = "./azure/modules/resource_group"
-  providers = { azurerm = azurerm.xpe_shared_poc
+  resourece_group_name = "my-resource-group"
+  location          = "South Central US"
+  tags              = {
+    environment = "production"
+    project     = "my-project"
   }
-  resourece_group_name = "myResourceGroup2"
-  location = "East US"
-  tags = {Environment = "Production", "Owner" = "TeamA"}
-  
 }
 
-
-#
