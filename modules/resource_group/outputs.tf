87f09d64743c0533105dc5ea0419f6662ff25dd7
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 17:11:32 -0600

    v11

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -1,15 +1,15 @@
+# modules/resource_group/outputs.tf
 output "resource_group_name" {
-  value       = module.resource_group.resource_group_name
   description = "Resource group name"
+  value       = azurerm_resource_group.this.name
 }
 
 output "resource_group_location" {
-  value       = module.resource_group.resource_group_location
   description = "Resource group location"
+  value       = azurerm_resource_group.this.location
 }
 
 output "id" {
-  value       = module.resource_group.id
   description = "Resource group ID"
+  value       = azurerm_resource_group.this.id
 }
-
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,15 @@
-output "resource_group_1" {
-  value       = module.resource_group_1.resource_group_name
+# root/outputs.tf
+output "resource_group_1_name" {
   description = "Resource group name"
+  value       = module.resource_group_1.resource_group_name
 }
 
 output "resource_group_1_id" {
-  value       = module.resource_group_1.resource_group_id
   description = "Resource group id"
+  value       = module.resource_group_1.id     # <-- antes ponías resource_group_id (no existe)
 }
 
 output "resource_group_1_location" {
-  value       = module.resource_group_1.resource_group_location
   description = "Resource group location"
+  value       = module.resource_group_1.resource_group_location
 }
