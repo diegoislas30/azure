d49986df64d8d2ef87fdfab317c4895d65198903
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 17:36:40 -0600

    nuevas configuraciones

--- main.tf
+++ main.tf
@@ -8,3 +8,50 @@ module "resource_group_1" {
   }
 }
 
+
+module "vnet_1" {
+  source              = "./modules/vnets"
+  vnet_name           = "vnet-ejemplo"
+  resource_group_name = module.resource_group_1.resource_group_name
+  location            = module.resource_group_1.resource_group_location
+  address_space       = ["20.0.0.0/16"]
+  dns                 = []
+  tags                = {
+    environment = "poc"
+    project     = "xpertal"
+
+  }
+
+  subnets = {
+    subnet1 = {
+      address_prefix = "20.10.0.0/24"
+      nsg_id         = null
+      route_table_id = null
+      service_endpoints = ["Microsoft.Storage", "Microsoft.Sql"]
+    }
+
+    subnet2 = {
+      address_prefix = "20.20.0.0/24"
+      nsg_id         = null
+      route_table_id = null
+      service_endpoints = []
+    }
+  }
+
+  providers = {
+    azurerm = azurerm.xpe_shared_poc
+  }
+
+  depends_on = [module.resource_group_1]
+
+}
+
+output "vnet_1_id" {
+  value       = module.vnet_1.vnet_id
+  description = "ID de la VNET creada"
+}
+
+output "subnet1_id" {
+  value       = module.vnet_1.subnet_ids["subnet1"]
+  description = "ID de la subnet1"
+}
--- modules/vnets/main.tf
+++ modules/vnets/main.tf
@@ -1,11 +1,33 @@
 resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
+  resource_group_name = var.resource_group_name
   location            = var.location
-  resource_group_name = var.vnet.resource_group_name
   address_space       = var.address_space
+  dns_servers         = var.dns
+  tags                = var.tags
+}
 
-  subnet {
-    name = var.subnet_name
-    address_prefix = var.address_prefix
-  }
+resource "azurerm_subnet" "this" {
+  for_each             = var.subnets
+  name                 = each.key
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [each.value.address_prefix]
+  service_endpoints    = lookup(each.value, "service_endpoints", [])
+
+  private_endpoint_network_policies_enabled      = lookup(each.value, "private_endpoint_network_policies", true)
+  private_link_service_network_policies_enabled  = lookup(each.value, "private_link_service_network_policies", true)
+}
+
+# (opcionales) asociaciones si pasas IDs
+resource "azurerm_subnet_network_security_group_association" "nsg" {
+  for_each = { for k, v in var.subnets : k => v if try(v.nsg_id, null) != null }
+  subnet_id                 = azurerm_subnet.this[each.key].id
+  network_security_group_id = each.value.nsg_id
+}
+
+resource "azurerm_subnet_route_table_association" "rt" {
+  for_each = { for k, v in var.subnets : k => v if try(v.route_table_id, null) != null }
+  subnet_id      = azurerm_subnet.this[each.key].id
+  route_table_id = each.value.route_table_id
 }
