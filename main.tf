5b2a83baac5a6db9a05e7cf0c4063eaa7af133d6
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 13:06:26 -0600

    vars

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "resource_group_1" {
   source              = "./modules/resource_group"
   resource_group_name = "rg-ejemplo"
   location            = "eastus"
+
   providers = {
     azurerm = azurerm.xpe_shared_poc
   }
---
+++ variables.tf
@@ -0,0 +1,8 @@
+# variables.tf (en el root)
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
