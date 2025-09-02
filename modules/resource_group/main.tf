de6ec09a8eb7fa49091ae2ecd5f67df1fdf84f85
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 21:50:57 -0600

    se corrige el modulo resources group}

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,6 +1,5 @@
 resource "azurerm_resource_group" "this" {
   name     = var.resource_group_name
   location = var.location
-  tags     = var.tags
+  tags     = tomap(var.tags)  # object -> map(string)
 }
-
