6f2e2075ffa7e353838367f67dc2a726b4d2f207
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-02 13:56:22 -0600

    se generan dos resource group

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "resource_group_xpe_rg_001" {
   tags = {
     UDN = "Xpertal"
     OWNER     = "Diego Enrique Islas Cuervo"
-    xpeowner = "dislas@caabsa.com.mx"
+    xpeowner = "diegoenrique.islas@xpertal.com"
     proyecto = "Terraform"
     ambiente = "DEV"
     
@@ -15,3 +15,23 @@ module "resource_group_xpe_rg_001" {
   providers = { azurerm = azurerm.xpe_shared_poc}
 
 }
+
+
+module "resource_group_xpe_rg_002" {
+  source              = "./modules/resource_group"
+  resource_group_name = "xpe-rg-002"
+  location            = "southcentralus"
+
+  tags = {
+    UDN = "Xpertal"
+    OWNER     = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "Terraform"
+    ambiente = "DEV"
+    
+  }
+
+  providers = { azurerm = azurerm.xpe_shared_poc}
+
+}
+
