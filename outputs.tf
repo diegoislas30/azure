5a790a77c11d00e6e91e5b9b139f16bbf0f66712
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-02 13:32:36 -0600

    test

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,20 @@
-## genera los output s de la infraestructura creada
-
 output "resource_group_name" {
-  description = "The name of the Resource Group"
-  value       = azurerm_resource_group.main.name
-}
-
-output "resource_group_id" {
-  description = "The ID of the Resource Group"
-  value       = azurerm_resource_group.main.id
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.resource_group_xpe_rg_001.name
 }
 
 output "resource_group_location" {
-  description = "The location of the Resource Group"
-  value       = azurerm_resource_group.main.location
+  description = "The location of the resource group"
+  value       = azurerm_resource_group.resource_group_xpe_rg_001.location
 }
 
+output "resource_group_id" {
+  description = "The ID of the resource group"
+  value       = azurerm_resource_group.resource_group_xpe_rg_001.id
+}
 
 output "resource_group_tags" {
-  description = "The tags assigned to the Resource Group"
-  value       = azurerm_resource_group.main.tags
+  description = "The tags of the resource group"
+  value       = azurerm_resource_group.resource_group_xpe_rg_001.tags
 }
 
