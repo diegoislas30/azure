a1ea0751cf9ad96df64c2464ed35257f03e1ce14
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-02 14:06:16 -0600

    se genera la primera vnet

--- main.tf
+++ main.tf
@@ -35,3 +35,32 @@ module "resource_group_xpe_rg_002" {
 
 }
 
+module "vnet_xpe_vnet_001" {
+  source              = "./modules/vnets"
+  vnet_name           = "xpe-vnet-001"
+  resource_group_name = "module.resource_group_xpe_rg_001.resource_group_name"
+  location            = "module.resource_group_xpe_rg_001.resource_group_location"
+
+  tags = {
+    UDN = "Xpertal"
+    OWNER     = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "Terraform"
+    ambiente = "DEV"
+  }
+
+  address_space = ["10.0.0.0/16"]
+
+  subnets = {
+    frontend = {
+      address_prefix = "10.0.1.0/24"
+      service_endpoints = ["Microsoft.Storage"]
+      nsg_id = null
+      route_table_id = null
+      private_endpoint_network_policies = true
+      private_link_service_network_policies = true
+      service_endpoints = ["Microsoft.Storage"]
+
+    }
+  }
+}
