37f30fb14475439f0cacded9bd01cc2f921976b2
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-02 13:16:51 -0600

    se modifica rg

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 # root/outputs.tf
-output "resource_group_1_name" {
+output "resource_group_xpe_rg_001_name" {
   description = "Resource group name"
   value       = module.resource_group_1.resource_group_name
 }
 
-output "resource_group_1_id" {
+output "resource_group_xpe_rg_001_id" {
   description = "Resource group id"
   value       = module.resource_group_1.id     # <-- antes ponías resource_group_id (no existe)
 }
 
-output "resource_group_1_location" {
+output "resource_group_xpe_rg_001_location" {
   description = "Resource group location"
   value       = module.resource_group_1.resource_group_location
 }
