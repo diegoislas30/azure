d94b24393162898177c29f7b1b7f87c5e9e459c4
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-02 14:17:09 -0600

    prueba10

--- main.tf
+++ main.tf
@@ -35,12 +35,14 @@ module "resource_group_xpe_rg_002" {
 
 }
 
+
+
 module "vnet_xpe_vnet_001" {
   source              = "./modules/vnets"
   vnet_name           = "xpe-vnet-001"
-  resource_group_name = "module.resource_group_xpe_rg_001.resource_group_xpe_rg_001_name"
-  location            = "module.resource_group_xpe_rg_001.resource.group_xpe_rg_001_location"
-
+  resource_group_name = module.resource_group_xpe_rg_001.resource_group_name
+  location            = module.resource_group_xpe_rg_001.resource_group_location
+  address_space       = ["10.0.0.0/16"]
   tags = {
     UDN = "Xpertal"
     OWNER     = "Diego Enrique Islas Cuervo"
@@ -48,9 +50,6 @@ module "vnet_xpe_vnet_001" {
     proyecto = "Terraform"
     ambiente = "DEV"
   }
-
-  address_space = ["10.0.0.0/16"]
-
   subnets = {
     frontend = {
       address_prefix = "10.0.1.0/24"
@@ -60,9 +59,8 @@ module "vnet_xpe_vnet_001" {
       private_endpoint_network_policies = true
       private_link_service_network_policies = true
       service_endpoints = ["Microsoft.Storage"]
-
     }
   }
-
   providers = { azurerm = azurerm.xpe_shared_poc}
 }
+  
