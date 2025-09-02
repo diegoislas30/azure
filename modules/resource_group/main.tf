fc9306bc5f34995963dbb616f737859ddc55bd6e
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-01 21:53:25 -0600

    prueba

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,5 +1,6 @@
 resource "azurerm_resource_group" "this" {
   name     = var.resource_group_name
   location = var.location
-  tags     = tomap(var.tags)  # object -> map(string)
+  tags     = tomap(var.tags)  # object -> map(string) se pasan de objeto a mapa
 }
+
