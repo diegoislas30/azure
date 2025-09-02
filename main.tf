c33c3c66aba389d81adf9b2e6a727543307cd64a
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-02 15:30:34 -0600

    vm test

--- main.tf
+++ main.tf
@@ -63,4 +63,30 @@ module "vnet_xpe_vnet_001" {
   }
   providers = { azurerm = azurerm.xpe_shared_poc}
 }
+
+# manda a llamar al modulo de maquina virtual
+module "vm_xpe_vm_001" {
+  source              = "./modules/virtual-machine"
+  vm_name             = "xpe-vm-001"
+  resource_group_name = module.resource_group_xpe_rg_001.resource_group_name
+  location            = module.resource_group_xpe_rg_001.resource_group_location
+  vm_size             = "Standard_B2s"
+  # en las variables de admin y password... trae las variables por defecto declaradas en el variables.tf del modulo
+  admin_username      = []
+  admin_password      = []
+  subnet_id           = module.vnet_xpe_vnet_001.subnet_ids["frontend"]
+  os_disk_size_gb     = 127
+  data_disks = []
+  tags = {
+    UDN = "Xpertal"
+    OWNER     = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "Terraform"
+    ambiente = "DEV"
+  }
   
+
+  providers = { azurerm = azurerm.xpe_shared_poc}
+
+
+}
