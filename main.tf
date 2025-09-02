80501530d56b1f5d157781fae19da80fc58ea429
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-02 13:30:04 -0600

    test

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 module "resource_group_xpe_rg_001" {
   source              = "./modules/resource_group"
   resource_group_name = "xpe-rg-001"
-  location            = "East US"
+  location            = "southcentralus"
 
   tags = {
-    UDN = "xpertal"
-    OWNER = "Diego Islas"
-    xpeowner = "diegoenrique.islas@xpertal.com"
-    proyecto = "Terraform"
-    ambiente = "Dev"
+    UDN         = "Xpertal"
+    OWNER       = "Diego Islas"
+    xpeowner    = "diegoenrique.islas@xpertal.com"
+    proyecto    = "Terraform"
+    ambiente    = "Dev"
+  
   }
-
-  providers = { azurerm = azurerm.xpe_shared_poc }
+}
+    
