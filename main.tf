9ab0767248bf8615925e84d9c291707b7a1196c5
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-02 16:17:18 -0600

    rollback

--- main.tf
+++ main.tf
@@ -65,47 +65,9 @@ module "vnet_xpe_vnet_001" {
 }
 
 
-# NSG genérico
-module "nsg_1" {
-  source              = "./modules/nsg"
-  nsg_name            = "xpe-nsg-001"
-  resource_group_name = module.resource_group_xpe_rg_001.resource_group_name
-  location            = module.resource_group_xpe_rg_001.resource_group_location
 
-  tags = {
-    UDN      = "Xpertal"
-    OWNER    = "Diego Enrique Islas Cuervo"
-    xpeowner = "diegoenrique.islas@xpertal.com"
-    proyecto = "Terraform"
-    ambiente = "DEV"
-  }
 
-  security_rules = {
-    allow_http = {
-      priority                   = 110
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "80"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-    allow_https = {
-      priority                   = 120
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "443"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-  }
-
-  providers = { azurerm = azurerm.xpe_shared_poc }
-}
-
+  
 # VM que usa ese NSG (asociado a la NIC)
 module "virtual_machine_xpe_vm_001" {
   source              = "./modules/virtual-machine"
