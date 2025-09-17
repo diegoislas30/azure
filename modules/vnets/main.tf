568cf6e1cf63007a56f5b7a528094778f3a625da
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-17 14:25:09 -0600

    deploy

--- modules/vnets/main.tf
+++ modules/vnets/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
 # === VNET ===
 resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
