ba71750ac65d2481358efd122811592121c7cfd8
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-17 15:51:23 -0600

    se deployan nsg

--- main.tf
+++ main.tf
@@ -143,6 +143,54 @@ module "resource_group_xpeterraformpoc3" {
 # }
 
 
+module "network_security_group" {
+  source = "./modules/network_security_group"
+
+  nsg_name            = "xpeterraformpoc-nsg"
+  resource_group_name = module.resource_group_xpeterraformpoc2.resource_group_name
+  location            = module.resource_group_xpeterraformpoc2.resource_group_location
+
+  security_rules = [
+    {
+      name                       = "Allow-HTTP"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "Allow-HTTPS"
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+
+  tags = {
+    UDN      = "Xpertal"
+    OWNER    = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "terraform"
+    ambiente = "dev"
+  }
+
+  providers = {
+    azurerm = azurerm.xpe_shared_poc
+  }
+
+}
+
+
+
 
 
 
