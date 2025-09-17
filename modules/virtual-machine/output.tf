275855df3e5462503c0a0358730536aee3a23e07
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-17 16:23:08 -0600

    deploy vm test 2

--- modules/virtual-machine/output.tf
+++ modules/virtual-machine/output.tf
@@ -1,14 +1,9 @@
 output "vm_id" {
-  description = "ID de la VM"
-  value       = azurerm_windows_virtual_machine.this.id
-}
-
-output "nic_id" {
-  description = "ID de la NIC"
-  value       = azurerm_network_interface.this.id
+  description = "ID de la VM Linux creada"
+  value       = azurerm_linux_virtual_machine.this.id
 }
 
 output "private_ip" {
-  description = "IP privada de la VM"
-  value       = azurerm_network_interface.this.private_ip_address
+  description = "Dirección IP privada de la VM"
+  value       = azurerm_network_interface.this.ip_configuration[0].private_ip_address
 }
