a490ba621159aedb9a76eabc6bf1bf9ed4e22c1f
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-17 15:59:00 -0600

    se deploya vnet

--- main.tf
+++ main.tf
@@ -190,6 +190,90 @@ module "network_security_group" {
 }
 
 
+module "network_security_group2" {
+  source = "./modules/network_security_group"
+
+  nsg_name            = "xpeterraformpoc-nsg2"
+  resource_group_name = module.resource_group_xpeterraformpoc3.resource_group_name
+  location            = module.resource_group_xpeterraformpoc3.resource_group_location
+
+  security_rules = [
+    {
+      name                       = "Allow-HTTP"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "Allow-HTTPS"
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+
+
+  tags = {
+    UDN      = "Xpertal"
+    OWNER    = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "terraform"
+    ambiente = "dev"
+  }
+
+  providers = {
+    azurerm = azurerm.xpe_shared_poc
+  }
+
+}
+
+# === Virtual Network ===
+
+module "vnet_xpeterraformpoc2" {
+  source = "./modules/vnets"
+
+  vnet_name           = "xpeterraformpoc2-vnet"
+  location            = module.resource_group_xpeterraformpoc2.resource_group_location
+  resource_group_name = module.resource_group_xpeterraformpoc2.resource_group_name
+  address_space       = ["20.0.0.0/16"]
+  subnets = [
+    {
+      name           = "default"
+      address_prefix = "20.0.10.0/24"
+    },
+    {
+      name           = "web"
+      address_prefix = "20.0.20.0/24"
+    },
+    {
+      name           = "app"
+      address_prefix = "20.0.30.0/24"
+    }
+  ]
+
+  tags = {
+    UDN      = "Xpertal"
+    OWNER    = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "terraform"
+    ambiente = "dev"
+  }
+
+  providers = {
+    azurerm = azurerm.xpe_shared_poc
+  }
+
+}
 
 
 
