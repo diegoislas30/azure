c92ea6e0ca4d84262dd52b46435988ce58202dbc
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-17 19:32:12 -0600

    final 3

--- modules/network_security_group/variables.tf
+++ modules/network_security_group/variables.tf
@@ -31,3 +31,24 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "security_rules" {
+  description = "Lista de reglas de seguridad para el NSG"
+  type = list(object({
+    name                        = string
+    priority                    = number
+    direction                   = string
+    access                      = string
+    protocol                    = string
+    source_port_range           = string
+    destination_port_range      = string
+    source_address_prefix       = string
+    destination_address_prefix  = string
+  }))
+  default = []
+}
+
+variable "nsg_name" {
+  description = "Nombre del Network Security Group"
+  type        = string
+}
