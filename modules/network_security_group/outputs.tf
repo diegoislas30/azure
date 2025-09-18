c9a4c4f72136584e0640f34e364d0d619e19716d
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-17 19:03:09 -0600

    test final 11

--- modules/network_security_group/outputs.tf
+++ modules/network_security_group/outputs.tf
@@ -1,9 +1,15 @@
-output "nsg_id" {
+output "id" {
   description = "ID del Network Security Group."
   value       = azurerm_network_security_group.this.id
 }
 
-output "nsg_name" {
+output "name" {
   description = "Nombre del Network Security Group."
   value       = azurerm_network_security_group.this.name
 }
+
+output "rules" {
+  description = "Reglas de seguridad del NSG"
+  value       = { for r in azurerm_network_security_rule.this : r.name => r.id }
+}
+
