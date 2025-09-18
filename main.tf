983b105aa356b833dfc9695a85a2d245f0fe72f7
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-09-18 11:52:42 -0600

    asociación de vnet

--- main.tf
+++ main.tf
@@ -313,7 +313,52 @@ tags = {
     }
 }
 
+
+module "network_security_group_hub" {
+  source              = "./modules/network_security_group"
+  nsg_name            = "xpeterraformpoc-hub-nsg"
+  resource_group_name = module.resource_group_hub.resource_group_name
+  location            = module.resource_group_hub.resource_group_location
+
+  security_rules = [
+    {
+      name                       = "Allow-HTTP"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "Allow-HTTPS"
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+
+  tags = {
+    UDN      = "Xpertal"
+    OWNER    = "Diego Islas"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "terraform"
+    ambiente = "qa"
+    }
+
+    providers = {
+        azurerm = azurerm.xpe_shared_poc
+    }
+
     
 
 
-  
+
+
