e28b25df32c666592f38e82f7887fd542ce8733e
Diego Enrique islas Cuervo <diego@Diegos-MacBook-Pro.local>
2025-09-26 10:10:18 -0600

    v12

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
 import {
-  to = module.resource_group.azurerm_resource_group.this["terraform-import-test"]
+  to = module.resource_group.azurerm_resource_group.this
   id = "/subscriptions/bc444e87-bfcd-4aeb-93f9-9a52b1089062/resourceGroups/terraform-import-test"
 }
