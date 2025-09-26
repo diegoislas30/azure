9c5dda1b43ffcdd50dbd2af9b082ccb54bf7a606
Diego Enrique islas Cuervo <diego@Diegos-MacBook-Pro.local>
2025-09-26 10:14:18 -0600

    test15

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-output "hub_vnet_id" {
-  value = module.vnet_hub.vnet_id
-}
-
-output "hub_subnet_ids" {
-  value = module.vnet_hub.subnet_ids
-}
-
-output "spoke_vnet_id" {
-  value = module.vnet_spoke.vnet_id
-}
-
-output "spoke_subnet_ids" {
-  value = module.vnet_spoke.subnet_ids
+output "resource_group_name" {
+  value = module.resource_group.resource_group_name
 }
