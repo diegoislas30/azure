6934c5e814fac0280ba8a937eb86172f46f5e4b8
Diego Enrique islas Cuervo <diego@Diegos-MacBook-Pro.local>
2025-09-26 16:59:37 -0600

    se agregan outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,20 @@ output "resource_group_name" {
 output "vnet_id" {
   value = module.vnet_simple.vnet_id
 }
+
+output "vnet_name" {
+  value = module.vnet_simple.vnet_name
+}
+
+output "vnet_location" {
+  value = module.vnet_simple.vnet_location
+}
+
+output "subnet_id" {
+  value = module.vnet_simple.subnet_id
+}
+
+output "subnet_name" {
+  value = module.vnet_simple.subnet_name
+}
+
