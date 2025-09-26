4504dfffd5973451d2c30219840a26ba9292588d
Diego Enrique islas Cuervo <diego@Diegos-MacBook-Pro.local>
2025-09-26 11:08:45 -0600

    añadir nsg

--- main.tf
+++ main.tf
@@ -400,3 +400,46 @@ module "resource_group" {
   }
 }
 
+module "network_security_group" {
+  source              = "./modules/network_security_group"
+  nsg_name            = "terraform-import-test-nsg"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+
+  security_rules = [
+    {
+      name                       = "Allow-HTTP"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "Allow-HTTPS"
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "443"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+
+  tags = {
+    UDN      = "Xpertal"
+    OWNER    = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "terraform"
+    ambiente = "dev"
+  }
+  providers = {
+    azurerm = azurerm.xpe_shared_poc
+  }
+}
+
