f169809351ee2042dacd8b01a00f4669924a0f2f
Diego Enrique islas Cuervo <diego@Diegos-MacBook-Pro.local>
2025-09-26 16:47:45 -0600

    se importa grupo de recursos a terraform

--- main.tf
+++ main.tf
@@ -465,3 +465,24 @@
 # }
 
 ## =================================================================== ##
+
+
+module "resource_group" {
+  source = "./modules/resource_group"
+
+  resource_group_name = "terraform-import"
+  location            = "southcentralus"
+  tags = {
+    UDN      = "Xpertal"
+    OWNER    = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "terraform"
+    ambiente = "dev"
+  }
+
+    providers = {
+        azurerm = azurerm.xpe_shared_poc
+    }
+
+}
+
