748ee95e963d09a1520a900a1b578ccb3fb27152
Diego Enrique islas Cuervo <diego@Diegos-MacBook-Pro.local>
2025-09-26 16:54:32 -0600

    se crea una vnet dentro de el resoruce group importado

--- main.tf
+++ main.tf
@@ -486,3 +486,37 @@ module "resource_group" {
 
 }
 
+## == Ejemplo de declarar el modulo de vnet sin peerings ni delegaciones ==
+module "vnet_simple" {
+    source              = "./modules/vnets"
+    vnet_name           = "vnet-terraform-import"
+    location            = module.vnet_simple.resource_group_location
+    resource_group_name = module.vnet_simple.resource_group_name
+    address_space       = ["10.10.0.0/16"]
+
+    subnets = [
+        {
+            name           = "servidores"
+            address_prefix = "10.10.1.0/24"
+        },
+        {
+            name           = "bases de datos"
+            address_prefix = "10.10.2.0/24"
+        }
+    ]
+
+    tags = {
+        UDN      = "Xpertal"
+        OWNER    = "Diego Islas"
+        xpeowner = "diegoenrique.islas@xpertal.com"
+        proyecto = "terraform"
+        ambiente = "dev"
+    }
+
+    providers = {
+        azurerm = azurerm.xpe_shared_poc
+    }
+}
+
+
+
