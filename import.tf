40843a9840e172741cc09510727d35af0af270d8
Diego Enrique Islas Cuervo <diegoenrique.islas@xpertal.com>
2025-09-28 11:46:01 -0600

    se añaden los readme

--- import.tf
+++ import.tf
@@ -1,5 +1,8 @@
-import {
-  to = module.resource_group.azurerm_resource_group.this
-  id = "/subscriptions/bc444e87-bfcd-4aeb-93f9-9a52b1089062/resourceGroups/terraform-import"
-}
+## En este archivo se define la importación de recursos existentes en Azure a la configuración de Terraform.
+
+
+#import {
+#  to = module.resource_group.azurerm_resource_group.this
+#  id = "/subscriptions/bc444e87-bfcd-4aeb-93f9-9a52b1089062/resourceGroups/terraform-import"
+#}
 
