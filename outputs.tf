c20ee6ce7af866db4c0c896467cac402d9e094f8
Diego Enrique Islas Cuervo <diegoenrique.islas@xpertal.com>
2025-09-28 13:08:19 -0600

    recursos de prueba

--- outputs.tf
+++ outputs.tf
@@ -13,20 +13,13 @@ output "resource_group_id" {
   value       = module.resource_group_xpeterraformpoc.resource_group_id
 }
 
-output "network_security_group_name" {
-  description = "The name of the Network Security Group"
-  value       = module.network_security_group.nsg_name
-}
 
 output "network_security_group_id" {
   description = "The ID of the Network Security Group"
   value       = module.network_security_group.nsg_id
 }
 
-output "network_security_group2_name" {
-  description = "The name of the second Network Security Group"
-  value       = module.network_security_group_2.nsg_name
-}
+
 
 output "network_security_group2_id" {
   description = "The ID of the second Network Security Group"
