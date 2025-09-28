90e05bad55ee3be2655fc3c5b800dab57e6a6a3b
Diego Enrique Islas Cuervo <diegoenrique.islas@xpertal.com>
2025-09-28 12:23:04 -0600

    recursos de prueba

--- main.tf
+++ main.tf
@@ -1,51 +1,6 @@
 
 
-## == Ejemplo de declarar el modulo de network security group ==
 
-
-# module "network_security_group" {
-#   source              = "./modules/network_security_group"
-#   nsg_name            = "xpeterraformpoc2-nsg"
-#   resource_group_name = module.resource_group_xpeterraformpoc2.resource_group_name
-#   location            = module.resource_group_xpeterraformpoc2.resource_group_location
-#
-#   security_rules = [
-#     {
-#       name                       = "Allow-HTTP"
-#       priority                   = 100
-#       direction                  = "Inbound"
-#       access                     = "Allow"
-#       protocol                   = "Tcp"
-#       source_port_range          = "*"
-#       destination_port_range     = "80"
-#       source_address_prefix      = "*"
-#       destination_address_prefix = "*"
-#     },
-#     {
-#       name                       = "Allow-HTTPS"
-#       priority                   = 110
-#       direction                  = "Inbound"
-#       access                     = "Allow"
-#       protocol                   = "Tcp"
-#       source_port_range          = "*"
-#       destination_port_range     = "443"
-#       source_address_prefix      = "*"
-#       destination_address_prefix = "*"
-#     }
-#   ]
-#
-#   tags = {
-#     UDN      = "Xpertal"
-#     OWNER    = "Guillermo Yam"
-#     xpeowner = "guillermo.yam@xpertal.com"
-#     proyecto = "terraform"
-#     ambiente = "dev"
-#   }
-#
-#   providers = {
-#     azurerm = azurerm.xpe_shared_poc
-#   }
-# }
 
 ## =================================================================== ##
 
@@ -568,7 +523,7 @@ module "resource_group_xpeterraformpoc" {
    }
  }
 
- module "network_security_group" {
+ module "network_security_group2" {
    source              = "./modules/network_security_group"
    nsg_name            = "xpeterraformpoc2-nsg"
    resource_group_name = module.resource_group_xpeterraformpoc2.resource_group_name
