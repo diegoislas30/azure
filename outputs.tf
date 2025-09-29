3781a25a00935e48d553925abcf534d4c268da36
Diego Enrique Islas Cuervo <diegoenrique.islas@xpertal.com>
2025-09-28 20:00:55 -0600

    se añaden readme restantes

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,2 @@
-output "resource_group_name" {
-  description = "The name of the resource group"
-  value       = module.resource_group_xpeterraformpoc.resource_group_name
-}
 
-output "resource_group_location" {
-  description = "The location of the resource group"
-  value       = module.resource_group_xpeterraformpoc.resource_group_location
-}
-
-output "resource_group_id" {
-  description = "The ID of the resource group"
-  value       = module.resource_group_xpeterraformpoc.resource_group_id
-}
-
-
-output "network_security_group_id" {
-  description = "The ID of the Network Security Group"
-  value       = module.network_security_group.nsg_id
-}
 
