3435225842958d3d20388650a4ec1d8e558e4d45
Diego Enrique Islas Cuervo <diegoenrique.islas@xpertal.com>
2025-09-29 13:12:09 -0600

    test 20

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "resource_group_name" {
-  description = "The name of the resource group"
-  value       = resource_group_xpeterraformpoc.resource_group_name
+output "xp_rg_name" {
+  description = "Nombre del Resource Group"
+  value       = module.resource_group_xpeterraformpoc.resource_group_name
 }
 
-output "resource_group_location" {
-  description = "The location of the resource group"
-  value       = module.resource_group_xpeterraformpoc.location
+output "xp_rg_id" {
+  description = "ID del Resource Group"
+  value       = module.resource_group_xpeterraformpoc.resource_group_id
 }
 
-output "resource_group_id" {
-  description = "The ID of the resource group"
-  value       = module.resource_group_xpeterraformpoc.id
+output "xp_rg_location" {
+  description = "Location del Resource Group"
+  value       = module.resource_group_xpeterraformpoc.resource_group_location
 }
 
