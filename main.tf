c3aece1544e3a87b8bf42ecf0c6296f26566daba
Diego Enrique Islas Cuervo <diegoenrique.islas@xpertal.com>
2025-09-29 12:47:06 -0600

    test-rg

--- main.tf
+++ main.tf
@@ -1,20 +1,13 @@
-module "resource_group_terraform" {
-  source   = "./modules/resource_group"
+module "rg_terraform_vm" {
+  source              = "./modules/resource_group"
   resource_group_name = "rg-terraform-vm"
-  location = "southcentralus"
-  
-    tags = {
-        UDN = "Xpertal"
-        OWNER = "Diego Enrique Islas Cuervo"
-        xpeowner = "diegoenrique.islas@xpertal.com"
-        environment = "dev"
-        project = "Terraform"
-    }
+  location            = "southcentralus"
 
-    providers = {
-      azurerm = azurerm.xpe_shared_poc
+    tags = {
+        UDN      = "Xpertal"
+        OWNER    = "Diego Enrique Islas Cuervo"
+        xpeowner = "diegoenrique.islas@xpertal.com"
+        proyecto = "terraform-vm"
+        ambiente = "dev"
     }
 }
-
-
-
