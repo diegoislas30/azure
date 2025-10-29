c69d7baadfc32729d4473adf4ac6954bc7861855
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-10-29 08:14:10 -0600

    se cambia rg del container

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform"
+    resource_group_name  = "XPERTAL-Shared-xcs-ti-rg"
     storage_account_name = "xpeterraformpoc"
     container_name       = "terraform-tfstate"
     # el key lo definimos en CI / init
