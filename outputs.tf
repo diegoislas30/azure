e22e16a31d62246c1c229ab734392c922112897f
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-11-01 19:15:47 -0600

    outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,18 @@ output "terraform_rg_location" {
   description = "Location del Resource Group"
   value       = module.resource_group.resource_group_location
 }
+
+output "terraform_rg_name_2" {
+  description = "Nombre del Resource Group"
+  value       = module.resource_group_xpeterraformpoc_2.resource_group_name
+}
+
+output "terraform_rg_id_2" {
+  description = "ID del Resource Group"
+  value       = module.resource_group_xpeterraformpoc_2.resource_group_id
+}
+
+output "terraform_rg_location_2" {
+  description = "Location del Resource Group"
+  value       = module.resource_group_xpeterraformpoc_2.resource_group_location
+}
