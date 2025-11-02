5a9be5391ca41b738be75df96b3472f8022ffe46
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-11-01 19:10:57 -0600

    creación de otro rg

--- main.tf
+++ main.tf
@@ -13,3 +13,19 @@ module "resource_group" {
     azurerm = azurerm.XPERTAL-Shared-xcs
   }
 }
+
+module "resource_group_xpeterraformpoc_2" {
+  source = "./modules/resource_group"
+  resource_group_name = "terraform-import-2"
+  location = "eastus"
+  tags = {
+    UDN = "Xpertal"
+    OWNER = "Diego Enrique Islas Cuervo"
+    xpeowner = "diegoenrique.islas@xpertal.com"
+    proyecto = "terraform"
+    ambiente = "dev"
+  }
+  providers = {
+    azurerm = azurerm.XPERTAL-Shared-xcs
+  }
+}
