3b59d53e4731f8090e7ffb046c83f1139defb5e3
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-11-10 13:58:57 -0600

    se lanza primera vnet

--- main.tf
+++ main.tf
@@ -13,3 +13,38 @@ module "rg-scxpeicmprd" {
     azurerm = azurerm.Xpertal_XCS
   }
 }
+
+ module "vnetxpeicm-prd" {
+   source              = "./modules/vnets"
+   vnet_name           = vnetxpeicm-prd
+   location            = module.rg-scxpeicmprd.resource_group_location
+   resource_group_name = module.rg-scxpeicmprd.resource_group_name
+   address_space       = ["172.29.80.160/27"]
+
+   subnets = [  
+
+      {
+        name           = "snet-xpeicm-prd"
+        address_prefix = "172.29.80.192/27"
+        service_endpoints = []
+        delegations       = []
+        private_endpoint_network_policies_enabled = false
+      }
+   ]
+
+    tags = {
+      UDN      = "Xpertal"
+      OWNER    = "Martha Ibarra"
+      xpeowner = "martha.ibarra@xpertal.com"
+      proyecto = "ICM"
+      ambiente = "Productivo"
+    }
+
+    providers = {
+      azurerm = azurerm.Xpertal_XCS
+    }
+ }
+
+ 
+   
+
