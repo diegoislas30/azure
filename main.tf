b27ce87e45a1ff68facaeba2119a6ef3d4e58c5e
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-11-10 12:45:57 -0600

    se crea rg-scxpeicmprd

--- main.tf
+++ main.tf
@@ -1,23 +1,15 @@
-
 module "rg-scxpeicmprd" {
-   source = "./modules/resource_group"
-
-   resource_group_name = "rg-scxpeicmprd"
-   location            = "southcentralus"
-
-   tags = {
-     UDN      = "Xpertal"
-     OWNER    = "Martha Ibarra"
-     xpeowner = "martha.ibarra@xpertal.com"
-     proyecto = "ICM"
-     ambiente = "Productivo"
-   }
-
-   providers = {
-     alias   = azurerm.Xpertal_XCS
-
-   }
-
+  source = "./modules/resource_group"
+  resource_group_name = "rg-scxpeicmprd"
+  location            = "southcentralus"
+  tags = {
+    UDN      = "Xpertal"
+    OWNER    = "Martha Ibarra"
+    xpeowner = "martha.ibarra@xpertal.com"
+    proyecto = "ICM"
+    ambiente = "Productivo"
+  }
+  providers = {
+    azurerm = azurerm.Xpertal_XCS
+  }
 }
-
-
