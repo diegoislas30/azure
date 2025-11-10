ed91dcd300ec62923f0eef94db520dcb66295f4c
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-11-10 12:40:48 -0600

    se crea rg-scxpeicmprd

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ module "rg-scxpeicmprd" {
    }
 
    providers = {
-     azurerm = azurerm.Xpertal_XCS
+     alias   = azurerm.Xpertal_XCS
+
    }
- }
+
+}
 
 
-# Para importar el resource group definido por el módulo, ejecutar en terminal:
-# terraform import module.rg-scxpeicmprd.azurerm_resource_group.this /subscriptions/6d94fbd2-8182-4943-a9b3-53d236df5469/resourceGroups/rg-scxpeicmprd
