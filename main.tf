441af010fe21814bac1711f73b4acb72bce65d4c
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-11-10 14:31:34 -0600

    se añade vnet xpe-vneticmsqlmidb-prd

--- main.tf
+++ main.tf
@@ -45,6 +45,35 @@ module "rg-scxpeicmprd" {
     }
  }
 
+module "xpe-vneticmsqlmidb-prd" {
+   source              = "./modules/vnets"
+   vnet_name           = "xpe-vneticmsqlmidb-prd"
+   location            = module.rg-scxpeicmprd.resource_group_location
+   resource_group_name = module.rg-scxpeicmprd.resource_group_name
+   address_space       = ["172.29.80.192/27"]
 
+   subnets = [  
+
+      {
+        name           = "snet-xpeicm-prd"
+        address_prefix = "172.29.80.192/27"
+        service_endpoints = []
+        delegations       = []
+        private_endpoint_network_policies_enabled = false
+      }
+   ]
+
+    tags = {
+      UDN      = "Xpertal"
+      OWNER    = "Martha Ibarra"
+      xpeowner = "martha.ibarra@xpertal.com"
+      proyecto = "ICM"
+      ambiente = "Productivo"
+    }
+
+    providers = {
+      azurerm = azurerm.Xpertal_XCS
+    }
+ }
    
 
